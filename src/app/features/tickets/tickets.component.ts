135c1579c5dca7be99a636ff159ff726196e1a91
ast2u <carloasto11@gmail.com>
2025-03-13 16:14:51 +0800

    Add ticket component

---
+++ src/app/features/tickets/tickets.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets',
+  imports: [],
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.css'
+})
+export class TicketsComponent implements OnInit {
+
+  ngOnInit(): void {
+      
+  }
+}
