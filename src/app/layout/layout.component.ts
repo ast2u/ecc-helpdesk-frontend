378ee7a3faaec21dda9f3f53b5f77d406e4b978f
Carlo Astudillo <carloasto11@gmail.com>
2025-03-16 19:00:53 +0800

    Refactor the design for layout navigation

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { AuthService } from '../core/service/auth.service';
+import { AuthService } from '../core/services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
