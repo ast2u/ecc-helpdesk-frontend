22eb9a51577aa360a86bc46286b75e9d799efe4f
Carlo Astudillo <carloasto11@gmail.com>
2025-03-17 21:28:04 +0800

    Add ticket service and work on the views

--- src/app/shared/model/tickets.ts
+++ src/app/shared/model/tickets.ts
@@ -1,2 +1,10 @@
-export interface Tickets {
+import { Base } from "./base";
+import { Employees } from "./employee/employees";
+
+export interface Tickets extends Base{
+    ticketNumber: string,
+    title: string,
+    body: string,
+    status: string,
+    assignee: Employees
 }
