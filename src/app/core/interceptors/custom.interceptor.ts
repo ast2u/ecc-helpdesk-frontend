dfb16e266e96a83120968eee0e2a69c318f23a19
ast2u <carloasto11@gmail.com>
2025-03-18 15:47:51 +0800

    Add new changes for ticket service

--- src/app/core/interceptors/custom.interceptor.ts
+++ src/app/core/interceptors/custom.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const customInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
   if(token){
-    console.log(JSON.parse(atob(token.split('.')[1])));
+    console.log(JSON.parse(atob(token.split('.')[1]))); // should be removed  
     const clonedReq = req.clone ({
       setHeaders: {
         Authorization: `Bearer ${token}`
--- src/app/features/tickets/tickets.service.ts
+++ src/app/features/tickets/tickets.service.ts
@@ -1,12 +1,35 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TicketSearchRequest } from '../../shared/model/tickets/ticket-search-request';
+import { Observable } from 'rxjs';
+import { Tickets } from '../../shared/model/tickets/tickets';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketsService {
-
+  private apiUrl = 'http://localhost:8080/api/tickets';
   constructor(private http: HttpClient) { }
 
+  getAllEmployees(searchRequest: TicketSearchRequest, page = 0, size = 0): Observable<{ content: Tickets[], page: any }> {
+      const params = this.buildParams(searchRequest, page, size);
+      return this.http.get<{ content: Tickets[], page: any }>(this.apiUrl, { params });
+    }
   
+  private buildParams(searchRequest: TicketSearchRequest, page: number, size: number): HttpParams {
+    let params = new HttpParams().set('page', page).set('size', size);
+
+    Object.entries(searchRequest).forEach(([key, value]) => {
+      if (value !== undefined && value !== null && value !== '') {
+        if (Array.isArray(value)) {
+          value.forEach(val => params = params.append(key, val));
+        } else {
+          params = params.set(key, value.toString());
+        }
+      }
+    });
+
+    return params;
+  }
+
 }
