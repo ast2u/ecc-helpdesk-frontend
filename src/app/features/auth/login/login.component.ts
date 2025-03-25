e53a9a5e22d7b0f63796b2e9dfcf6e79cd39b498
ast2u <carloasto11@gmail.com>
2025-03-25 15:34:48 +0800

    Add loading for login

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -16,18 +17,22 @@ export class LoginComponent {
     "username": "",
     "password": ""
   }
+  isLoading = false;
   
   authService = inject(AuthService)
   router = inject(Router)
   snackBar = inject(MatSnackBar);
 
   onLogin() {
+    this.isLoading = true;
     this.authService.login(this.loginObj.username, this.loginObj.password).subscribe({
       next: (data) => {
         if (data) {
           this.showSnackbar(data.message,'success-snackbar')
+          this.isLoading = false;
           this.router.navigate(['dashboard']); // Redirect to dashboard
         } else {
+          this.isLoading = false;
           this.showSnackbar(data.message,'error-snackbar')
         }
       },
